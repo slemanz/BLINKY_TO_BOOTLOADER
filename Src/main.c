ebad883af09f417bcf71a43987efe7087d0f8452
slemanz <william_sleman@hotmail.com>
2024-08-19 05:33:50 -0300

    project(feat): add first code blinky code

--- Src/main.c
+++ Src/main.c
@@ -2,35 +2,43 @@
 
 
 // Function prototype
-void delay(volatile uint32_t count);
+void delay_cycles(uint32_t cycles);
+
+// Define led
+#define LED_PORT        GPIOA
+#define LED_PIN         GPIO_PIN_NO_3
+
 
 void gpio_setup(void)
 {
     // Set GPIOA pin 3 as output
+    GPIO_Handle_t GpioLed;
+	GpioLed.pGPIOx = LED_PORT;
+	GpioLed.GPIO_PinConfig.GPIO_PinNumber = LED_PIN;
+	GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_LOW;
+	GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	GpioLed.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
 
-
+    GPIO_Init(&GpioLed);
+    GPIO_WriteToOutputPin(LED_PORT, LED_PIN, GPIO_PIN_SET);
 }
 
 
 int main(void)
  {
+    gpio_setup();
 
-    while (1) {
-
-        // Delay
-        delay(4000000); // Adjust the delay value as necessary
+    while (1)
+    {
+        GPIO_ToggleOutputPin(LED_PORT, LED_PIN);
+        delay_cycles(16000000 / 4);
     }
 }
 
-void delay(volatile uint32_t count) {
-    while (count-- > 0) {
+void delay_cycles(uint32_t cycles)
+{
+    while (cycles-- > 0) {
         __asm("NOP"); // No operation for delay
-    }
-}
-
-void reset_handler(void) {
-    // Initialization code
-    while (1) {
-        // Main loop
     }
 }
