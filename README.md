95f39ad618bc7701a183b4b9002c49a1a8a6e024
slemanz <william_sleman@hotmail.com>
2024-09-06 05:33:02 -0300

    project(complete): heartbeat project

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Welcome to the **Blinky to Bootloader** repository! This project is dedicated to
 In this repository, you will find simple examples starting with the classic "Blinky" program. As you progress, you'll learn how to implement UART communication, and eventually, develop a bootloader capable of firmware updates. Each code example is well-documented to ensure you can follow along easily and grasp the low-level interaction with the hardware.
 
 Clone the repository, set up your toolchain, and start coding! This project is designed for anyone looking to enhance their skills in embedded programming, whether you're a beginner or an experienced engineer.
+
+Inspired in [Low Byte Productions](https://www.youtube.com/@LowByteProductions) series.
--- Src/main.c
+++ Src/main.c
@@ -35,22 +35,33 @@ int main(void)
 
     uint64_t start_time = system_get_ticks();
     float duty_cycle = 0.0f;
+    uint8_t flag_up = 1;
+    float duty_step = 1.0f;
+    uint32_t delayMs = 15;
 
     while (1)
     {
-        if((system_get_ticks() - start_time) >= 10)
+        if((system_get_ticks() - start_time) >= delayMs)
         {
-            duty_cycle += 1;            
+
+            duty_cycle = (flag_up) ? duty_cycle + duty_step : duty_cycle - duty_step;
 
             if(duty_cycle >= 100.0f)
             {
+                flag_up = 0;
+                duty_cycle = 100.0f;
+            }
+            else if(duty_cycle <= 0)
+            {
+                flag_up = 1;
+                duty_step = 1.0f;
                 duty_cycle = 0.0f;
             }
-
+            
             timer_pwm_set_duty_cycle(&PWM, duty_cycle);
             start_time = system_get_ticks();
         }
 
-        // Do useful work
+        // Do other stuff
     }
 }
