3ee966808332fafa5c9dfd08349f5ece81f0336f
slemanz <william_sleman@hotmail.com>
2024-09-11 05:20:53 -0300

    project(feat): create bootloader folder

---
+++ bootloader/Src/core/system.c
@@ -0,0 +1,41 @@
+#include "core/system.h"
+
+static volatile uint64_t ticks = 0;
+
+void SysTick_Handler(void)
+{
+    ticks++;
+}
+
+uint64_t system_get_ticks(void)
+{
+    return ticks;
+}
+
+void systick_setup(void)
+{
+    systick_set_frequency(SYSTICK_FREQ, CPU_FREQ);
+    systick_counter_enable();
+    systick_interrupt_enable();
+}
+
+void system_setup(void)
+{
+    systick_setup();
+}
+
+void gpio_setup(void)
+{
+    // Set GPIOA pin 3 as output
+    GPIO_Handle_t GpioLed;
+	GpioLed.pGPIOx = LED_PORT;
+	GpioLed.GPIO_PinConfig.GPIO_PinNumber = LED_PIN;
+	GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_ALTFN;
+    GpioLed.GPIO_PinConfig.GPIO_PinAltFunMode = PA5_ALTFN_TIM2_CH1;
+	GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_LOW;
+	GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	GpioLed.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+    GPIO_Init(&GpioLed);
+}
+
