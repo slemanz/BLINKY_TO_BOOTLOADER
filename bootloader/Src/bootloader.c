c0e902ae73495056a059d9836bc9157b3a138e4a
slemanz <william_sleman@hotmail.com>
2024-09-13 05:23:43 -0300

    project(feat): try to flash bootloader, but not work

--- bootloader/Src/bootloader.c
+++ bootloader/Src/bootloader.c
@@ -5,11 +5,11 @@
 #define FLASH_BASE                  (0x08000000UL)
 #define MAIN_APP_START_ADDRESS      (FLASH_BASE + BOOTLOADER_SIZE)
 
-void jump_to_main(void)
+static void jump_to_main(void)
 {
     typedef void (*void_fn)(void);
     uint32_t* reset_vector_entry = (uint32_t*)(MAIN_APP_START_ADDRESS + 4U);
-    uint32_t* reset_vector = (uint32_t)(*reset_vector_entry);
+    uint32_t* reset_vector = (uint32_t*)(*reset_vector_entry);
 
     void_fn jump_fn = (void_fn)reset_vector;
     jump_fn();
