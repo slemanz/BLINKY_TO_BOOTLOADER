1867a5f569bea2795092533f1462361531e1e1f8
slemanz <william_sleman@hotmail.com>
2024-09-14 11:04:07 -0300

    project(feat): adjust to get map file in build

--- app/Src/main.c
+++ app/Src/main.c
@@ -32,16 +32,15 @@ int main(void)
     system_setup();
     timer_setup();
 
-    //uint64_t start_time = system_get_ticks();
-    //float duty_cycle = 0.0f;
-    //uint32_t delayMs = 100;
+    uint64_t start_time = system_get_ticks();
+    float duty_cycle = 0.0f;
+    uint32_t delayMs = 100;
 
     while (1)
     {
-        /*
         if((system_get_ticks() - start_time) >= delayMs)
         {
-            duty_cycle += 10;
+            duty_cycle += 10.0f;
             if(duty_cycle >= 100.0f)
             {
                 duty_cycle = 0.0f;
@@ -49,7 +48,7 @@ int main(void)
             
             timer_pwm_set_duty_cycle(&PWM, duty_cycle);
             start_time = system_get_ticks();
-        }*/
+        }
 
         // Do other stuff
     }
