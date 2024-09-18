83ff17ef45bb73513f29c6a6cf7f5be3dbe0eb7b
slemanz <william_sleman@hotmail.com>
2024-09-18 05:30:49 -0300

    project(feat): begin setup uart function

--- shared/Src/core/uart.c
+++ shared/Src/core/uart.c
@@ -1,9 +1,28 @@
 #include "stm32f401xx.h"
 
+static uint16_t compute_uart_div(uint32_t PeriphClk, uint32_t BaudRate);
+
+static uint16_t compute_uart_div(uint32_t PeriphClk, uint32_t BaudRate)
+{
+    return ((PeriphClk + (BaudRate/2U))/BaudRate);
+}
 
 void uart_setup(void)
 {
     UART2_PCLK_EN();
+
+    // no flow control (default reset)
+    UART2->CR1 &= ~(1 << 12); // 8 databits
+    UART2->BRR = compute_uart_div(16000000, 115200); // baurate
+    UART2->CR1 &= ~(1 << 10); // parity disable
+    UART2->CR2 &= ~(0x3 << 12); // 1 stop bits
+
+    // tx en
+    // rx en
+
+
+    // do macros to bit shifts
+    // enable interrupt to rx
 }
 
 void uart_write(uint8_t* data, const uint32_t lenght)
