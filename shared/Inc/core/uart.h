e9b77f4af2a0b6618ef6ca46bac01b48b90a0c82
slemanz <william_sleman@hotmail.com>
2024-09-19 05:08:39 -0300

    project(feat): add it config in uart

--- shared/Inc/core/uart.h
+++ shared/Inc/core/uart.h
@@ -7,8 +7,10 @@
  *  it position definions Uart
  */
 
+#define UART_CR1_UE             13
 #define UART_CR1_M              12
 #define UART_CR1_PCE            10
+#define UART_CR1_RXNEIE          5
 #define UART_CR1_TE              3
 #define UART_CR1_RE              2
 
@@ -30,5 +32,7 @@ void uart_write_byte(uint8_t data);
 uint32_t uart_read(uint8_t *data, const uint32_t length);
 uint8_t uart_read_byte(void);
 bool uart_data_available(void);
+
+void UART_IRQITConfig(uint8_t IRQNumber, uint8_t EnorDi);
 
 #endif /* INC_UART_H_ */
--- shared/Src/core/uart.c
+++ shared/Src/core/uart.c
@@ -21,8 +21,10 @@ void uart_setup(void)
     UART2->CR1 |= (1 << UART_CR1_RE); // rx en
 
 
-    // do macros to bit shifts
-    // enable interrupt to rx
+    UART2->CR1 |= (1 << UART_CR1_RXNEIE); // enable interrupt to rx
+    UART_IRQITConfig(38, ENABLE);
+
+    UART2->CR1 |= (1 << UART_CR1_UE);// enable uart periph
 }
 
 void uart_write(uint8_t* data, const uint32_t lenght)
@@ -48,4 +50,44 @@ uint8_t uart_read_byte(void)
 bool uart_data_available(void)
 {
 
+}
+
+
+void UART_IRQITConfig(uint8_t IRQNumber, uint8_t EnorDi)
+{
+    // reference: CORTEX M4 user guide
+	if(EnorDi == ENABLE)
+	{
+		if(IRQNumber <= 31)
+		{
+			// program ISER0 register
+			*NVIC_ISER0 |= (1 << IRQNumber);
+
+		}else if(IRQNumber > 31 && IRQNumber < 64) // 32 to 63
+		{
+			// program ISER1 register
+			*NVIC_ISER1 |= (1 << (IRQNumber % 32));
+
+		}else if(IRQNumber >= 64 && IRQNumber < 96) // 64 to 95
+		{
+			// program ISER2 register
+			*NVIC_ISER2 |= (1 << (IRQNumber % 64));
+
+		}
+	}else
+	{
+		if(IRQNumber <= 31)
+		{
+			// program ICER0 register
+			*NVIC_ICER0 |= (1 << IRQNumber);
+		}else if(IRQNumber > 31 && IRQNumber < 64)
+		{
+			// program ICER1 register
+			*NVIC_ICER1 |= (1 << (IRQNumber % 32));
+		}else if(IRQNumber >= 64 && IRQNumber < 96)
+		{
+			// program ICER2 register
+			*NVIC_ICER2 |= (1 << (IRQNumber % 64));
+		}
+	}
 }
