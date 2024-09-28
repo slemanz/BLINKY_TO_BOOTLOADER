f77688c8c25de9f2183a5715c0a570b23c46c7eb
slemanz <william_sleman@hotmail.com>
2024-09-28 11:58:13 -0300

    project(feat): begin to write ring buffer functions

--- shared/Inc/core/ring-buffer.h
+++ shared/Inc/core/ring-buffer.h
@@ -5,7 +5,7 @@
 
 typedef struct ring_buffer_t
 {
-    uint8_t* buffer;
+    uint8_t  *buffer;
     uint32_t mask;
     uint32_t read_index;
     uint32_t write_index;
@@ -13,8 +13,8 @@ typedef struct ring_buffer_t
 
 void ring_buffer_setup(ring_buffer_t *rb, uint8_t *buffer, uint32_t size);
 bool ring_buffer_empty(ring_buffer_t *rb);
+bool ring_buffer_read(ring_buffer_t *rb, uint8_t *byte);
 bool ring_buffer_write(ring_buffer_t *rb, uint8_t byte);
-bool ring_buffer_read(ring_buffer_t *rb, uint8_t byte);
 
 
 #endif /* INC_RING_BUFFER_H_ */
