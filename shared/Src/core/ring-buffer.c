b7acaccb288bd033b44ed67777092418ffa018d1
slemanz <william_sleman@hotmail.com>
2024-09-28 15:58:21 -0300

    project(feat): add ring buffer write function

--- shared/Src/core/ring-buffer.c
+++ shared/Src/core/ring-buffer.c
@@ -36,6 +36,18 @@ bool ring_buffer_read(ring_buffer_t *rb, uint8_t *byte)
 
 bool ring_buffer_write(ring_buffer_t *rb, uint8_t byte)
 {
+    uint32_t local_write_index = rb->write_index;
+    uint32_t local_read_index = rb->read_index;
 
+    uint32_t next_write_index = (local_write_index + 1) & rb->mask;
+
+    if(next_write_index == local_read_index)
+    {
+        return false;
+    }
+
+    rb->buffer[local_write_index] = byte;
+    rb->write_index = next_write_index;
+    return true;
 }
 
