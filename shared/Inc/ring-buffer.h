90ccbfbfa4405b12765bfb34d7dc7756c5ff6cb0
slemanz <william_sleman@hotmail.com>
2024-09-28 05:49:18 -0300

    project(feat): add funtions prototypes

--- shared/Inc/ring-buffer.h
+++
@@ -1,4 +0,0 @@
-#ifndef INC_RING_BUFFER_H_
-#define INC_RING_BUFFER_H_
-
-#endif /* INC_RING_BUFFER_H_ */
--- shared/Src/core/ring-buffer.c
+++ shared/Src/core/ring-buffer.c
@@ -1,1 +1,27 @@
 #include "stm32f401xx.h"
+#include "core/ring-buffer.h"
+
+
+
+void ring_buffer_setup(ring_buffer_t *rb, uint8_t *buffer, uint32_t size)
+{
+
+}
+
+
+bool ring_buffer_empty(ring_buffer_t *rb)
+{
+
+}
+
+
+bool ring_buffer_write(ring_buffer_t *rb, uint8_t byte)
+{
+
+}
+
+
+bool ring_buffer_read(ring_buffer_t *rb, uint8_t byte)
+{
+
+}
