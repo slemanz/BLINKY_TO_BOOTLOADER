685672ae370979e712f39745ffb6f78a76ee09fd
slemanz <william_sleman@hotmail.com>
2024-10-04 03:10:36 -0300

    project(feat): begin packet protocol api

---
+++ bootloader/Src/comms.c
@@ -0,0 +1,26 @@
+#include "comms.h"
+
+void comms_setup(void)
+{
+
+}
+
+void comms_update(void)
+{
+
+}
+
+bool comms_packets_available(void)
+{
+
+}
+
+void comms_write(comms_packet_t *packet)
+{
+
+}
+
+void comms_read(comms_packet_t *packet)
+{
+
+}
