38140f41d731a3c1f0075be32f9b71045816ac60
slemanz <william_sleman@hotmail.com>
2024-10-04 04:54:31 -0300

    project(feat): begin state machine to packet update

--- bootloader/Src/comms.c
+++ bootloader/Src/comms.c
@@ -1,4 +1,17 @@
 #include "comms.h"
+#include "uart.h"
+
+typedef enum comms_state_t
+{
+    CommsState_Length,
+    CommsState_Data,
+    CommsState_CRC
+} comms_state_t;
+
+static comms_state_t state = CommsState_Length;
+static uint8_t data_byte_count = 0;
+
+static comms_packet_t temporary_packet = {.length = 0, .data = {0}, .crc = 0};
 
 void comms_setup(void)
 {
@@ -7,7 +20,31 @@ void comms_setup(void)
 
 void comms_update(void)
 {
+    while(uart_data_available())
+    {
+        switch (state)
+        {
+            case CommsState_Length:
+            {
+                temporary_packet.length = uart_read_byte();
+                state = CommsState_Data;
+            }break;
+            case CommsState_Data:
+            {
+                temporary_packet.data[data_byte_count++] = uart_read_byte();
+                if(data_byte_count >= PACKET_DATA_LENGTH)
+                {
+                    data_byte_count = 0;
+                    state = CommsState_CRC;
+                }
 
+            }break;
+            case CommsState_CRC:
+            {
+
+            }break;
+        }
+    }
 }
 
 bool comms_packets_available(void)
