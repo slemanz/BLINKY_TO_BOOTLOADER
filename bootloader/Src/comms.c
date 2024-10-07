e8c831538e4b7866a9d7a1c17807d8603ff99af5
slemanz <william_sleman@hotmail.com>
2024-10-07 03:13:01 -0300

    project(feat): add more logics to comms update function

--- bootloader/Src/comms.c
+++ bootloader/Src/comms.c
@@ -14,11 +14,47 @@ static uint8_t data_byte_count = 0;
 
 static comms_packet_t temporary_packet = {.length = 0, .data = {0}, .crc = 0};
 static comms_packet_t retx_packet = {.length = 0, .data = {0}, .crc = 0};
+static comms_packet_t ack_packet = {.length = 0, .data = {0}, .crc = 0};
+
+static bool comms_is_retx_packet(const comms_packet_t *packet)
+{
+    if(packet->length != 1)
+    {
+        return false;
+    }
+
+    if(packet->data[0] != PACKET_RETX_DATA0)
+    {
+        return false;
+    }
+
+    for(uint8_t i = 1; i < PACKET_DATA_LENGTH; i++)
+    {
+        if(packet->data[i] != 0xff)
+        {
+            return false;
+        }
+    }
+}
 
 void comms_setup(void)
 {
-    
+    retx_packet.length = 1;
+    retx_packet.data[0] = PACKET_RETX_DATA0;
+    for(uint8_t i = 1; i < PACKET_DATA_LENGTH; i++)
+    {
+        retx_packet.data[i] = 0xff;
+    }
+    retx_packet.crc = comms_compute_crc(&retx_packet);
 
+
+    ack_packet.length = 1;
+    ack_packet.data[0] = PACKET_ACK_DATA0;
+    for(uint8_t i = 1; i < PACKET_DATA_LENGTH; i++)
+    {
+        ack_packet.data[i] = 0xff;
+    }
+    ack_packet.crc = comms_compute_crc(&ack_packet);
 }
 
 void comms_update(void)
@@ -48,7 +84,8 @@ void comms_update(void)
 
                 if(temporary_packet.crc != comms_compute_crc(&temporary_packet))
                 {
-                    
+                    comms_write(&retx_packet);
+                    state = CommsState_Length;
                 }
 
             }break;
