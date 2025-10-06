978470bcbc6f8159808dea17d6c9311671bc596a
slemanz <william_sleman@hotmail.com>
2025-10-06 04:29:42 -0300

    app(refact): improve code

--- app/Src/main.c
+++ app/Src/main.c
@@ -3,6 +3,8 @@
 
 #include "config_app.h"
 
+#include "driver_gpio.h"
+
 
 // Function delay
 void delay_cycles(uint32_t cycles)
@@ -12,43 +14,23 @@ void delay_cycles(uint32_t cycles)
     }
 }
 
-
-TIM_Handle_t PWM;
-
-void timer_setup(void)
-{
-    PWM.pTIMx = TIM2;
-    PWM.TIM_Config.channel = TIM_CHANNEL1;
-    PWM.TIM_Config.initialDuty = 100.0f;
-
-    // setup frequency and resolution
-    PWM.TIM_Config.prescaler = PRESCALER; // divided by 10
-    PWM.TIM_Config.auto_reload = ARR_VALUE; // divided by 26667 -> close to 60Hz
-
-    timer_pwm_init(&PWM);
-}
-
 int main(void)
  {
     system_setup();
-    gpio_setup();
-    uart_pin_setup();
+    config_drivers();
     uart_setup(); // 9600
     //timer_setup();
 
     uint64_t start_time = system_get_ticks();
     uint64_t start_time2 = system_get_ticks();
-    uint32_t delayMs = 1000;
 
     uint8_t string_send[] = "hello world!!!\r\n";
 
-    //float duty_cycle = 0.0f;
-    //timer_pwm_set_duty_cycle(&PWM, duty_cycle);
-
     while (1)
     {
-        if((system_get_ticks() - start_time) >= delayMs)
+        if((system_get_ticks() - start_time) >= 1000)
         {
+            GPIO_ToggleOutputPin(GPIOC, GPIO_PIN_NO_13);
             start_time = system_get_ticks();
         }
 
