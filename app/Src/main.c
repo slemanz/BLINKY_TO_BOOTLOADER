f64ef66e2b76ebf4e81a8c2a9736ceda8e2ea29c
slemanz <william_sleman@hotmail.com>
2025-10-12 07:31:54 -0300

    app(feat): add timebase interface

--- app/Src/main.c
+++ app/Src/main.c
@@ -1,48 +1,32 @@
-#include "stm32f411xx.h"
 #include <stdio.h>
 
 #include "config_app.h"
 
 // interface
 #include "interface_io.h"
-
-#include "driver_systick.h"
 #include "interface_comm.h"
-
-// Function delay
-void delay_cycles(uint32_t cycles)
-{
-    while (cycles-- > 0) {
-        __asm("NOP"); // No operation for delay
-    }
-}
+#include "interface_timebase.h"
 
 int main(void)
  {
     config_drivers();
     config_interface();
 
-    uint64_t start_time = ticks_get();
-    uint64_t start_time2 = ticks_get();
+    Timebase_Interface_t *ticks = timebase_get();
+    uint64_t start_time = ticks->get();
 
     printf("\n");
-    printf("Init board\n");
+    printf("Init app\n");
 
     Comm_Interface_t *serial = Comm_ProtocolGet(PROTOCOL_UART2);
     uint8_t data_rcv = 0;
 
     while (1)
     {
-        if((ticks_get() - start_time) >= 1000)
+        if((ticks->get() - start_time) >= 1000)
         {
             IO_Interface_get(IO0)->toggle();
-            start_time = ticks_get();
-        }
-
-        if((ticks_get() - start_time2) >= 10000)
-        {
-            printf("Hello\n");
-            start_time2 = ticks_get();
+            start_time = ticks->get();
         }
 
         while(serial->data_available())
