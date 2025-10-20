c71e2dcffd73c6c43ba4ad1d0f321a4ccabf6d11
slemanz <william_sleman@hotmail.com>
2025-10-20 04:05:55 -0300

    fw-updater(feat): begin main code

--- fw-updater/main.py
+++ fw-updater/main.py
@@ -1,22 +1,32 @@
 import serial
+from packet import packet_command
 
-def crc8(data):
-    crc = 0
-    for byte in data:
-        crc ^= byte
-        for _ in range(8):
-            if crc & 0x80:
-                crc = (crc << 1) ^ 0x07
-            else:
-                crc <<= 1
-    return (crc & 0xFF)
 
-print("Init fw-updater")
+print("INIT FW-UPDATER")
 
-with serial.Serial('/dev/ttyUSB0', baudrate=9600, timeout=20) as ser:
+with serial.Serial('/dev/ttyUSB0', baudrate=115200, timeout=2) as ser:
     print("Conectado")
     ser.flushInput()
     ser.flushOutput()
-    response = ser.read(10)
+
+    test = packet_command(0x2)
+    print(test)
+
+
+
+    ser.write("boot\n".encode('utf-8'))
+    response = ser.readline()
+    response = ser.readline()
+
+    if("BOOT".encode('utf-8') in response):
+        print("Boot mode")
+        ser.flushInput()
+    else:
+        print("Error trying to enter boot mode")
+        exit()
+
+    ser.write(test)
+    response = ser.read(18)
     print(response)
-    print(''.join(format(x, ' 02x') for x in response))
+
+    
