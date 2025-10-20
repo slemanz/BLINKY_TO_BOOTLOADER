08fcf17fd023bb88488ef7d754a05c6d9b157167
slemanz <william_sleman@hotmail.com>
2025-10-20 04:05:25 -0300

    fw-updater(feat): add crc module

---
+++ fw-updater/crc.py
@@ -0,0 +1,10 @@
+def crc8(data):
+    crc = 0
+    for byte in data:
+        crc ^= byte
+        for _ in range(8):
+            if crc & 0x80:
+                crc = (crc << 1) ^ 0x07
+            else:
+                crc <<= 1
+    return (crc & 0xFF)
