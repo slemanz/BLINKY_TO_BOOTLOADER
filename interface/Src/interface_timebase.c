245dcafbcda0e886825b1b4ce233d02e2a2f75df
slemanz <william_sleman@hotmail.com>
2025-10-24 04:49:21 -0300

    interface(refact): change timebase variable name

--- interface/Src/interface_timebase.c
+++ interface/Src/interface_timebase.c
@@ -1,12 +1,12 @@
 #include "interface_timebase.h"
 #include "driver_systick.h"
 
-const Timebase_Interface_t timer = {
+const Timebase_Interface_t timer_ticks = {
     .get = ticks_get,
     .delay = ticks_delay
 };
 
 Timebase_Interface_t *timebase_get(void)
 {
-    return (Timebase_Interface_t*)&timer;
+    return (Timebase_Interface_t*)&timer_ticks;
 }
