b6946db7f756d17c48f75658a59266d98bf7e7bf
slemanz <william_sleman@hotmail.com>
2025-10-25 04:28:22 -0300

    bootloader(refact): move jmp function and remove syscall

--- bootloader/Inc/config_boot.h
+++ bootloader/Inc/config_boot.h
@@ -4,9 +4,16 @@
 #include <stdint.h>
 #include <stddef.h>
 
+// bootloader size -> 32kB
+#define BOOTLOADER_SIZE             (0x8000UL)
+#define FLASH_BASE                  (0x08000000UL)
+#define MAIN_APP_START_ADDRESS      (FLASH_BASE + BOOTLOADER_SIZE)
+
 void config_drivers(void);
 void config_interface(void);
 
 void deinit_boot(void);
+void jump_to_main(void);
+
 
 #endif /* INC_CONFIG_BOOT_H_ */
--- bootloader/Src/bootloader.c
+++ bootloader/Src/bootloader.c
@@ -16,22 +16,6 @@
 // drivers
 #include "driver_flash.h"
 
-// bootloader size -> 32kB
-#define BOOTLOADER_SIZE             (0x8000UL)
-#define FLASH_BASE                  (0x08000000UL)
-#define MAIN_APP_START_ADDRESS      (FLASH_BASE + BOOTLOADER_SIZE)
-
-static void jump_to_main(void)
-{
-    typedef void (*void_fn)(void);
-    uint32_t* reset_vector_entry = (uint32_t*)(MAIN_APP_START_ADDRESS + 4U);
-    uint32_t* reset_vector = (uint32_t*)(*reset_vector_entry);
-
-    void_fn jump_fn = (void_fn)reset_vector;
-    jump_fn();
-}
-
-
 int main(void)
  {
     config_drivers();
