572df0928f62ba67f6001c445b93b1c5ca33c6ef
slemanz <william_sleman@hotmail.com>
2025-10-25 05:43:54 -0300

    common(feat): add more packet data0

--- common/Inc/comms/comms.h
+++ common/Inc/comms/comms.h
@@ -14,6 +14,17 @@
 #define PACKET_RETX_DATA0       (0x19)
 #define PACKET_ACK_DATA0        (0x15)
 
+#define BL_PACKET_SYNC_OBSERVED_DATA0       (0x20)
+#define BL_PACKET_FW_UPDATE_REQ_DATA0       (0x31)
+#define BL_PACKET_FW_UPDATE_RES_DATA0       (0x37)
+#define BL_PACKET_DEVICE_ID_REQ_DATA0       (0x3C)
+#define BL_PACKET_DEVICE_ID_RES_DATA0       (0x3F)
+#define BL_PACKET_FW_LENGTH_REQ_DATA0       (0x42)
+#define BL_PACKET_FW_LENGTH_RES_DATA0       (0x45)
+#define BL_PACKET_READY_FOR_DATA_DATA0      (0x48)
+#define BL_PACKET_UPDATE_SUCCESSFUL_DATA0   (0x54)
+#define BL_PACKET_NACK_DATA0                (0x59)
+
 typedef struct
 {
     uint8_t length;
